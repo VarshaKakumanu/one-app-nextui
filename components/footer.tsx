b6f9e2ec8cb5a17d2b425b394802051f565bec3a
VarshaKakumanu <varshakakumanu@gmail.com>
2024-02-26 08:31:17 +0530

    initital commit

---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { WhiteLogo } from "./icons";
+
+const Footer = () => {
+  return (
+    <div>
+      Footer
+      <WhiteLogo />
+    </div>
+  );
+};
+
+export default Footer;
