9d128679ffcd44367fe8d290308a49d2d6226496
VarshaKakumanu <varshakakumanu@gmail.com>
2024-02-29 08:53:22 +0530

    grid

--- components/card.tsx
+++ components/card.tsx
@@ -4,47 +4,48 @@ import { Card, CardBody, CardFooter, CardHeader } from "@nextui-org/card";
 import { Image } from "@nextui-org/image";
 import { Button } from "@nextui-org/button";
 import { HeartIcon } from "./icons";
+import { Chip } from "@nextui-org/chip";
 
 export default function Cardcomponent() {
   const list = [
     {
       title: "Orange",
-      img: "/images/fruit-1.jpeg",
+      img: "/mocks/mock1.jpeg",
       price: "$5.50",
     },
     {
       title: "Tangerine",
-      img: "/images/fruit-2.jpeg",
+      img: "/mocks/mock2.jpeg",
       price: "$3.00",
     },
     {
       title: "Raspberry",
-      img: "/images/fruit-3.jpeg",
+      img: "/mocks/mock3.jpeg",
       price: "$10.00",
     },
     {
       title: "Lemon",
-      img: "/images/fruit-4.jpeg",
+      img: "/mocks/mock4.jpeg",
       price: "$5.30",
     },
     {
       title: "Avocado",
-      img: "/images/fruit-5.jpeg",
+      img: "/mocks/mock5.jpeg",
       price: "$15.70",
     },
     {
       title: "Lemon 2",
-      img: "/images/fruit-6.jpeg",
+      img: "/mocks/mock6.jpeg",
       price: "$8.00",
     },
     {
       title: "Banana",
-      img: "/images/fruit-7.jpeg",
+      img: "/mocks/mock7.jpeg",
       price: "$7.50",
     },
     {
       title: "Watermelon",
-      img: "/images/fruit-8.jpeg",
+      img: "/mocks/mock8.jpeg",
       price: "$12.20",
     },
   ];
@@ -59,28 +60,14 @@ export default function Cardcomponent() {
           key={index}
           isPressable
           onPress={() => console.log("item pressed")}>
-          <CardHeader className='absolute z-10 top-1 flex-col items-start'>
-            <p className='text-tiny text-white/60 uppercase font-bold'>New</p>
-            <h4 className='text-black font-medium text-2xl'>Acme camera</h4>
-          </CardHeader>
-          <CardBody className='overflow-visible p-0'>
-            <Image
-              shadow='sm'
-              radius='lg'
-              width='100%'
-              alt={item.title}
-              className='w-full object-cover h-[140px]'
-              src={item.img}
-            />
-          </CardBody>
-          <CardFooter className='text-small justify-between'>
-            <b>{item.title}</b>
-            <p className='text-default-500'>{item.price}</p>
+          <CardHeader className='absolute z-20 top-1 flex justify-around'>
+            <Chip radius='sm'>Acme camera</Chip>
+
             <Button
               isIconOnly
               className='text-default-900/60 data-[hover]:bg-foreground/10 -translate-y-2 translate-x-2'
               radius='full'
-              variant='light'
+              variant='faded'
               onPress={() => setLiked((v) => !v)}>
               <HeartIcon
                 key={index}
@@ -88,6 +75,20 @@ export default function Cardcomponent() {
                 fill={liked ? "currentColor" : "none"}
               />
             </Button>
+          </CardHeader>
+          <CardBody className='overflow-visible p-0'>
+            <Image
+              shadow='sm'
+              radius='lg'
+              width='100%'
+              alt={item.title}
+              className='w-full object-cover h-[140px] bg-red-200'
+              src='/mocks/mock1.jpeg'
+            />
+          </CardBody>
+          <CardFooter className='text-small justify-between'>
+            <b>{item.title}</b>
+            <p className='text-default-500'>{item.price}</p>
           </CardFooter>
         </Card>
       ))}
